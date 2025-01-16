d1c500422b5291ac7b4c291589f8b62e9f21e299
BubbleShade <ozypfl@gmail.com>
2025-01-15 16:17:17 -0800

    Delete utils (Its all in beaverlib)

--- src/main/kotlin/frc/utils/AggregateException.kt
+++
@@ -1,7 +0,0 @@
-package com.team2898.engine.utils
-
-class AggregateException(message: String?, primary: Exception?, vararg others: Throwable) : Exception(message, primary) {
-    init {
-        others.forEach {err -> addSuppressed(err)}
-    }
-}
