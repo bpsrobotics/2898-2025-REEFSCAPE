b7c874ab3b43af7db134e25bd7595286803ddae9
Iprogramstuff1 <rudra.pokhariyal@gmail.com>
2025-02-07 18:23:20 -0800

    set a basic command to stop wrist movement, not meant to be working.

--- src/main/kotlin/frc/robot/commands/wristcommands/ConfigureWrist.kt
+++ src/main/kotlin/frc/robot/commands/wristcommands/ConfigureWrist.kt
@@ -1,4 +1,25 @@
 package frc.robot.commands.wristcommands
 
-class ConfigureWrist {
+
+
+import frc.robot.subsystems.Wrist
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj.Timer
+
+class ConfigureWrist: Command() {
+    val timer = Timer()
+    init { addRequirements(Wrist)}
+
+    override fun initialize() {
+
+    }
+
+    override fun execute() {
+        Wrist.feedbackController(0.0)
+    }
+
+    override fun isFinished(): Boolean {
+        return false
+    }
+
 }
