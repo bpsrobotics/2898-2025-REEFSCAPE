209f581e33787d09f135390b324ae3f173a59a89
Iprogramstuff1 <rudra.pokhariyal@gmail.com>
2025-02-15 12:57:23 -0800

    renamed stuff.

--- src/main/kotlin/frc/robot/commands/Sequences/StartupL1.kt
+++ src/main/kotlin/frc/robot/commands/Sequences/StartupL1.kt
@@ -1,7 +1,6 @@
 package frc.robot.commands.Sequences
  // a simple startup protocol, or if u wanna move it back down to L1
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.CommandScheduler
 import edu.wpi.first.wpilibj2.command.ScheduleCommand
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import frc.robot.Constants
@@ -9,7 +8,7 @@ import frc.robot.commands.wrist.MoveWrist
 import frc.robot.commands.elevator.MoveElevator
 import frc.robot.subsystems.Elevator
 
-class Startup : Command() {
+class StartupL1 : Command() {
     val commandGroup = SequentialCommandGroup(
         MoveWrist(Constants.PivotConstants.PivotState.Traverse.position),
         MoveElevator(Constants.ElevatorConstants.ElevatorState.Stow.position)
